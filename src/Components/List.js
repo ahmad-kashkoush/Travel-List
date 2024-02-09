db132b3a1afe3548ba00bf49a691abb1be2e845c
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-02-10 01:49:20 +0200

    Chore: line-through when item is packed

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,18 +1,37 @@
-export function List({ items, onDelete }) {
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
+export function List({ items, onDelete, onUpdate, onChangeList }) {
     return (
         <div className='list'>
             <ul>
-                {<>
-                    {items.map(item => <ListItem key={item} task={item} onDelete={onDelete} />)}
+                {<>{items
+                    .map(item => <ListItem
+                        key={uuidv4()}
+                        task={item}
+                        onDelete={onDelete}
+                        onUpdate={onUpdate}
+                    />)}
                 </>}
 
             </ul>
+            <Sort items={items} onChangeList={onChangeList} />
         </div>)
 }
-function ListItem({ task, onDelete }) {
+function ListItem({ task, onDelete, onUpdate }) {
+    const [isChecked, setIsChecked] = useState(task.packed);
+    const handleChecked = e => {
+        onUpdate({ ...task, packed: !isChecked })
+        setIsChecked((i) => !i);
+    }
     return <li>
-        <input id="chkBx" type="checkbox"></input>
-        <label for="chkBx">{task.amount}  {task.name}</label>
+        <input id={task.id} type="checkbox" defaultChecked={task.packed} onClick={handleChecked}></input>
+
+
+        <label
+            htmlFor={task.id}
+            style={isChecked ? { textDecoration: "line-through" } : { textDecoration: "none" }}>
+            {task.amount} {task.name}
+        </label>
         <button onClick={() => onDelete(task)}>❌</button>
     </li>
 }
