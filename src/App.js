f0122e1e9d89e88edc7a0bcdf554c68f231a289c
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-02-09 14:47:19 +0200

    initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './index copy.css';
+import { useState } from 'react';
+function App() {
+  return <div className='app'>
+    <Logo />
+  </div>;
+
+}
+function Logo() {
+  return <h1>Travel List App</h1>
+}
+export default App;
